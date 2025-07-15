b8102a2d2add472bba947c00c66adbfc97eaec47
lukerennebach <lucas.rennebach@gmail.com>
2025-07-15 18:26:59 +0200

    HTML adjusted with tags

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -3,29 +3,29 @@ import json
 with open("animals_template.html", "r") as fileobj:
     html = fileobj.read()
 
-
-
 def load_data(file_path):
     """ Loads a JSON file """
     with open(file_path, "r") as handle:
         return json.load(handle)
 
-
 def find_names(data):
     return [animal["name"] for animal in data]
-
 
 def find_diet(data):
     return [animal["characteristics"].get("diet", None) for animal in data]
 
-
 def find_location(data):
     return [animal["locations"][0] if animal["locations"] else None for animal in data]
-
 
 def find_type(data):
     return [animal["characteristics"].get("type", None) for animal in data]
 
+def write_new_html(html, output):
+    new_html = html.replace("__REPLACE_ANIMALS_INFO__", output)
+    output_path = "animals.html"
+    with open(output_path, "w") as handle:
+        handle.write(new_html)
+    print(f"New HTML written to {output_path}.")
 
 def main():
     data = load_data("animals_data.json")
@@ -37,17 +37,17 @@ def main():
 
     output = ""
     for name, diet, location, animal_type in zip(names, diets, locations, types):
-        output += f"Name: {name}\n"
+        output += f'<li class="cards__item">\n'
+        output += f"Name: {name}<br/>\n"
         if diet:
-            output += f"Diet: {diet}\n"
+            output += f"Diet: {diet}<br/>\n"
         if location:
-            output += f"Location: {location}\n"
+            output += f"Location: {location}<br/>\n"
         if animal_type:
-            output += f"Type: {animal_type}\n"
-        output += "\n"
+            output += f"Type: {animal_type}<br/>\n"
+        output += "</li>\n"
 
-
-    new_html = html.replace("__REPLACE_ANIMALS_INFO__", output)
+    write_new_html(html, output)
 
 if __name__ == "__main__":
     main()
